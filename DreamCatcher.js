a8aafa29d86509ef27be33d634311363cbe88320
Jamie Harvey <jharvey@duality.co.nz>
2019-11-13 15:02:20 +1300

    Added new amalgamated data structures

--- DreamCatcher.js
+++ DreamCatcher.js
@@ -1,12 +1,120 @@
+// TODO(j): move this to its own file(?)
+upgrades = {
+	money: {
+		dc: {
+			name: 'Dream Catcher',
+			description: 'A useless toy from the gypsy fair. Unless...',
+		 	cost: 20, 
+		 	available: false,
+		 	acquired: null,
+		 	onPickup: null,
+		 	runOnLoad: false
+		},
+		bed: {
+			name: 'New Bed',
+			description: 'The Deluxe Queen Supreme. A good night\'s sleep guaranteed, or your money back!',
+			cost: 400,
+			available: false
+			acquired: null,
+			onPickup: function() { gameData.muls.bed = 1 },
+		 	runOnLoad: false
+		}
+	}
+
+	goodDreams: {
+		elec: {
+			name: 'Generate Electricity?',
+			description: 'Surely there\'s <i>someway</i> to use this power...',
+		 	cost: 20, 
+		 	available: false,
+		 	acquired: null,
+		 	onPickup: function () {
+		 		// TODO(j): move this to the game loop
+		 		this.$nextTick(function () {
+                    window.setInterval(() => {
+                        if (gameData.goodDreams) {
+                        	gameData.elec += gameData.muls.elec;
+                        	gameData.goodDreams--;
+                        }
+                    }, gameData.periods.elec);
+                })
+		 	},
+		 	runOnLoad: false
+		},
+		sleepSchedule: {
+			name 'Sleeping Schedule',
+			description: '<q>Laziness casts into a deep sleep, And an idle man will suffer hunger.</q> Proverbs 19:15',
+			cost: 50,
+			available: false,
+			acquired: null,
+			onPickup: function() {
+		 		// TODO(j): move this to the game loop
+				this.$nextTick(function () {
+                    window.setInterval(() => {
+                        this.sleep();
+                    }, gameData.periods.sleep);
+                })
+			}
+		}
+	}
+}
+
+// TODO(j): move this to its own file(?)
+story = {
+	firstNight: {
+		lines: [
+			'As you lay down for your first nights sleep, you begin to think...'
+            'What if you could use your time asleep productively?',
+            '<q>Maybe tomorrow,</q> you think, as sleep over takes you.'
+        ],
+        trigger: {
+        	start: function() { return 10 },
+        	end: function() { return 10 },
+        },
+        onDisp: null
+    },
+    secondNight: {
+		lines: [
+            'Could I do something with my dreams?',
+            'It seems silly, absurd almost...',
+            'But why else would dream catchers even exist?',
+            'This has to be the use!'
+        ],
+        trigger: {
+        	start: function() { return 30 },
+        	end: function() { return upgrades.dc.acquired },
+        },
+        onDisp: function() { upgrades.dc.available = true }
+    },
+    elec: {
+    	lines: [
+            'The dream catcher is allowing me to generate power',
+            'By putting the power back into the grid, the power companys actually sending me money!!',
+            '<b>I NEED MORE!</b>'
+        ],
+        trigger: {
+        	start: function() { return upgrades.dc.acquired + 10 },
+        	end: function() { return upgrades.elec.acquired },
+        }
+        onDisp: function() { upgrades.elec.available = true }
+    }
+}
+
 gameData = {
     daysSlept: 0,
     goodDreams: 0,
     badDreams: 0,
     clickTime: 5000,
     dcPurchased: 0,
-    upgrades: {
-        electricity: false,
-        sleepSchedule: false
+    upgrades: upgrades,
+    story: story,
+    muls: {
+    	bed: 0.5,
+    	elec: 1
+    }
+    periods: {
+    	elec: 2000,
+    	sleep: 1000,
     }
 };
 
